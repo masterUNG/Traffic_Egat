3f127d1bdcc605e8bfd183c2fa4c8e1f5a2c919e
masterUNG <phrombutr@gmail.com>
2015-08-25 16:13:41 +0700

    นี่คือวันที่ 2

---
+++ app/src/main/java/appewtc/masterung/traffic/MyAdapter.java
@@ -0,0 +1,43 @@
+package appewtc.masterung.traffic;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by masterUNG on 8/25/15 AD.
+ */
+public class MyAdapter extends BaseAdapter{
+
+    //Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
+
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}   // Main Class
